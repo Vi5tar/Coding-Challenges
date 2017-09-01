977f67ef677c2c0a1489cab82d8bf301b87a7bc2
Travis Swiers <tswiers85@gmail.com>
2017-09-01 09:24:51 -0600

    Setup New Challenge

---
+++ Multiplication Tables (Codewars)/Multiplication Tables.js
@@ -0,0 +1,9 @@
+function multiplicationTable(row, col) {
+
+}
+
+multiplicationTable(2, 2) //should return [[1,2],[2,4]])
+multiplicationTable(3, 3) //should return [[1,2,3],[2,4,6],[3,6,9]])
+multiplicationTable(3, 4) //should return [[1,2,3,4],[2,4,6,8],[3,6,9,12]])
+multiplicationTable(4, 4) //should return [[1,2,3,4],[2,4,6,8],[3,6,9,12],[4,8,12,16]])
+multiplicationTable(2, 5) //should return [[1,2,3,4,5],[2,4,6,8,10]])
