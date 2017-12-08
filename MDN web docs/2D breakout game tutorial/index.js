d281f837e8000e8f115b38b66035168db583804a
Travis Swiers <tswiers85@gmail.com>
2017-12-08 16:42:23 -0700

    Completed Step 3
    * Ball bounces off the canvas walls
    * Changes color everytime it hits a wall

--- MDN web docs/2D breakout game tutorial/index.js
+++ MDN web docs/2D breakout game tutorial/index.js
@@ -1,14 +1,16 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var x = canvas.width/2;
-var y = canvas.height-30;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
+var ballColor = "#0095DD";
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI*2);
-  ctx.fillStyle = "#0095DD";
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = ballColor;
   ctx.fill();
   ctx.closePath();
 }
@@ -18,6 +20,14 @@ function draw() {
   drawBall();
   x += dx;
   y += dy;
+  if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+    ballColor = randomColor();
+  }
+  if (y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+    ballColor = randomColor();
+  }
 }
 
-setInterval(draw, 10);
+setInterval(draw, 20);
